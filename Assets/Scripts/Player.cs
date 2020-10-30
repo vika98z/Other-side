82072a82e5b51f1be166e195d0e0c4ba7fc095f7
vika98z <vika98z@mail.ru>
2020-10-30 03:22:22 +0300

    prototype

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerType { DarkPlayer, LightPlayer };
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject darkSkin;
+    [SerializeField]
+    private GameObject lightSkin;
+
+    public PlayerType PlayerType;
+
+    public void SetPlayerType(PlayerType type)
+    {
+        switch (type)
+        {
+            case PlayerType.DarkPlayer:
+                darkSkin.SetActive(true);
+                lightSkin.SetActive(false);
+                break;
+            case PlayerType.LightPlayer:
+                lightSkin.SetActive(true);
+                darkSkin.SetActive(false);
+                break;
+            default:
+                break;
+        }
+
+        PlayerType = type;
+    }
+}
---
+++ Assets/Scripts/WorldsController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public enum WorldType { Dark, Light };
+
+public class WorldsController : MonoBehaviour
+{
+    public WorldType CurrentWorldType { get; private set; }
+
+    [SerializeField]
+    private GameObject darkWorld;
+    [SerializeField]
+    private GameObject lightWorld;
+    [SerializeField]
+    private Player player;
+
+    void Awake()
+    {
+        player.SetPlayerType(PlayerType.LightPlayer);
+        darkWorld.SetActive(false);
+        lightWorld.SetActive(true);
+        CurrentWorldType = WorldType.Light;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ChangeWorlds();
+        }
+    }
+
+    private void ChangeWorlds()
+    {
+        switch (CurrentWorldType)
+        {
+            case WorldType.Dark:
+                darkWorld.SetActive(false);
+                lightWorld.SetActive(true);
+                player.SetPlayerType(PlayerType.LightPlayer);
+                CurrentWorldType = WorldType.Light;
+                break;
+            case WorldType.Light:
+                darkWorld.SetActive(true);
+                lightWorld.SetActive(false);
+                player.SetPlayerType(PlayerType.DarkPlayer);
+                CurrentWorldType = WorldType.Dark;
+                break;
+            default:
+                break;
+        }
+    }
+}
